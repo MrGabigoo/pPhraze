52e6fc048efe10440625a12a80aa37083c5d5cb8
Gabriel Bugeaud <gabrielbugeaud@gmail.com>
2016-08-05 00:58:35 +0200

    First version
    It works!

---
+++ source/PassphraseGenerator/Form1.Designer.vb
@@ -0,0 +1,129 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.RichTextBox1 = New System.Windows.Forms.RichTextBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.2!, CType((System.Drawing.FontStyle.Bold Or System.Drawing.FontStyle.Italic), System.Drawing.FontStyle), System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(160, 29)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(473, 20)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "pPhraze, a free and open-source passphrase generator"
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(327, 278)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(138, 41)
+        Me.Button1.TabIndex = 1
+        Me.Button1.Text = "Add word"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(11, 80)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(745, 170)
+        Me.Label2.TabIndex = 2
+        Me.Label2.Text = resources.GetString("Label2.Text")
+        '
+        'RichTextBox1
+        '
+        Me.RichTextBox1.Font = New System.Drawing.Font("Microsoft Sans Serif", 13.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.RichTextBox1.Location = New System.Drawing.Point(10, 325)
+        Me.RichTextBox1.Name = "RichTextBox1"
+        Me.RichTextBox1.ReadOnly = True
+        Me.RichTextBox1.Size = New System.Drawing.Size(769, 112)
+        Me.RichTextBox1.TabIndex = 3
+        Me.RichTextBox1.Text = ""
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(12, 302)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(159, 17)
+        Me.Label3.TabIndex = 4
+        Me.Label3.Text = "Generated Passphrase:"
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(704, 278)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(75, 41)
+        Me.Button2.TabIndex = 5
+        Me.Button2.Text = "Clear"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(498, 443)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(281, 17)
+        Me.Label4.TabIndex = 6
+        Me.Label4.Text = "Written by Gabriel Bugeaud (@MrGabigoo)"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(793, 469)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.RichTextBox1)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.Label1)
+        Me.MaximizeBox = False
+        Me.Name = "Form1"
+        Me.ShowIcon = False
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "pPhraze"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Label2 As Label
+    Friend WithEvents RichTextBox1 As RichTextBox
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Label4 As Label
+End Class
---
+++ source/PassphraseGenerator/Form1.vb
@@ -0,0 +1,37 @@
+﻿
+
+Imports System.IO
+
+Public Class Form1
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+
+        If File.Exists("input\list.txt") Then
+
+            Dim ioFile As New StreamReader("input\list.txt")    'StreamReader to read the file
+            Dim lines As New List(Of String)                    'Just a list to hold the lines
+            Dim rnd As New Random()                             'Random Class to generate a random number
+            Dim line As Integer                                 'Variable to hold random line number
+            While ioFile.Peek <> -1                             'loop through each line of the text file and add all the lines to the list
+                lines.Add(ioFile.ReadLine)
+            End While
+
+            line = rnd.Next(lines.Count + 1)                    'generate a random number
+            RichTextBox1.AppendText(lines(line).Trim() + " ")         'add it to the RichTextBox
+
+            'TODO: remove eventual spaces from the output
+
+            ioFile.Close()
+            ioFile.Dispose()
+
+        Else        'If file not found
+            MsgBox("Uh-oh." + Environment.NewLine + Environment.NewLine + "I wasn't able to open the file 'input\list.txt'" + Environment.NewLine + Environment.NewLine + "Could you please make sure this file exists?")
+        End If
+
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        RichTextBox1.Text = ""          'Clears contents of RichTextBox
+    End Sub
+
+End Class
